7a9dc8c94c9d25aaf2040b30ec1a2218f083a868
mgoldyn <mgoldyn@o2.pl>
2020-10-12 17:52:42 +0200

    fix

--- project_2/zad3/selekt/selekt.vhd
+++ project_2/zad3/selekt/selekt.vhd
@@ -7,10 +7,8 @@ entity selekt is
 end selekt;
 
 architecture Behavioral of selekt is
-    signal a_1, b_1, a_0, b_0: std_logic;
-
     begin
         with x select
-            y <= '0' when "011" | "100" | "110" | "111",
-                 '1' when others;
+             y <= '0' when "011" | "100" | "110" | "111",
+                  '1' when others;
 end Behavioral;
